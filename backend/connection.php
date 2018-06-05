f6ce33656d5489234dc85200da97fca346925f4a
Dakpaneel <BradleyOosterveen@hotmail.com>
2018-06-05 22:05:23 +0200

    Partial update to posts and login
    Added generating posts from database, auto sort by new
    
    Added login page

---
+++ backend/connection.php
@@ -0,0 +1,10 @@
+<?php
+// Connect to server
+$servername="localhost";
+$username="root";
+$password="admin";
+$database="lazarus";
+
+// Verbinding maken met de database
+$dbconn = mysqli_connect($servername, $username, $password, $database);
+?>
